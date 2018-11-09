c8bd04daf546a5c5af3e003e5534c5f02fc1df4c
c4t3rpilL4r <raphael.dearce1989@gmail.com>
2018-11-09 23:25:38 +0800

    064 Building a sticky navigation - Part 2

--- 07/resources/js/scripts.js
+++ 07/resources/js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-    
+    $('.js--section-features').waypoint(function(direction) { /* this will display the sticky nav */
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;' /* this will offset the sticky nav appearance by 60px */
+    });
 });
