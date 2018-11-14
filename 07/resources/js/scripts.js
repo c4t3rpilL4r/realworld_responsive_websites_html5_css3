827574fa2bee3b357f62a36260413b51b58c37a2
c4t3rpilL4r <raphael.dearce1989@gmail.com>
2018-11-14 14:36:30 +0800

    066 Adding animations on scroll

--- 07/resources/js/scripts.js
+++ 07/resources/js/scripts.js
@@ -40,4 +40,29 @@ $(document).ready(function() {
 			}
 		});
 	});
+
+	/* Animations on scroll */
+	$('.js--wp-1').waypoint(function(direction) { /* this will add the animation for the features class */
+		$('.js--wp-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-2').waypoint(function(direction) { /* this will add the animation for the app-screen class */
+		$('.js--wp-2').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-3').waypoint(function(direction) { /* this will add the animation for the features */
+		$('.js--wp-3').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-4').waypoint(function(direction) { /* this will add the animation for the features */
+		$('.js--wp-4').addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});
 });
